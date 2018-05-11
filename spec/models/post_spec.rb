c2347d09f7896b5d7bb602ca03cbe46deea7d957
skibo <skibo@fidor.de>
2018-05-11 11:54:57 +0300

    Implemented user connection with tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'creation' do
     before do
-      @post = Post.create(date: Date.today, rationale: 'Something')
+      @user = User.create(email: 'test@test.com', password: 'asdfasdf',
+                          password_confirmation: 'asdfasdf', first_name: 'John',
+                          last_name: 'Snow')
+      @post = Post.create(date: Date.today, rationale: 'Something', user_id: @user.id)
     end
     it 'can be created' do
       expect(@post).to be_valid
